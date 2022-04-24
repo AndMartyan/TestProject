f9446a457d7042aabe8ed76ee703aa79619d8b8a
Andrew Martyanov <666ytra@gmail.com>
2022-04-24 20:30:26 +0300

    Mission Complete

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u0!jy&e$*(*c(bel9+stv*hwpns9$(sy21r3qhwqr-%1sg77n#'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
